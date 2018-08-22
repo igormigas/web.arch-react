09845e6c68ba311e0f4f7b278f98662da8f9e2bb
DESKTOP-IGOR\Igor <igor.migasiewicz@gmail.com>
2018-08-22 15:55:20 +0200

    Cleaning and minor fixes

--- src/database/api.js
+++ src/database/api.js
@@ -19,8 +19,8 @@ api.axios.interceptors.response.use( response => {
 //
 // MODEL FUNCTIONS
 //
-api.fn.getAll 		= ()				=> api.get('/posts/?per_page=100');
+api.fn.getAll 		= ()				=> api.get('/posts/?per_page=100&filter[public]=true');
 api.fn.getID 			= ( id ) 		=> api.get(`/posts/?filter[id]=${id}`);
-api.fn.postForm 	= ( data ) 	=> api.post(`/posts`, data);
+//api.fn.postForm 	= ( data ) 	=> api.post(`/posts`, data);
 
 export default api.fn;
